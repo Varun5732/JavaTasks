1e391e8a50e698b1be42cae012ba41127cebc833
Varun5732 <varunsuresh89@gmail.com>
2023-07-05 18:57:57 +0530

    update

---
+++ Task8Question1.java
@@ -0,0 +1,17 @@
+package tasks;
+
+public class Task8Question1 {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 15;
+        int c = 10;
+        int d = 5;
+
+        int sumAB = a + b;
+        int sumCD = c + d;
+
+        if (sumAB > sumCD) {
+            System.out.println("The sum of a and b is greater than the sum of c and d.");
+        }
+    }
+}
